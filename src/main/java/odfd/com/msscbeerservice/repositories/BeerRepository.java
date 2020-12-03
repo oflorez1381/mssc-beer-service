026617ccffc3bf6cff332b3e3a9866496277d709
oflorez1381 <oscarflorez1381@gmail.com>
2020-12-03 14:35:03 -0500

    feat(58): added spring data jpa repository

---
+++ src/main/java/odfd/com/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package odfd.com.msscbeerservice.repositories;
+
+import odfd.com.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
