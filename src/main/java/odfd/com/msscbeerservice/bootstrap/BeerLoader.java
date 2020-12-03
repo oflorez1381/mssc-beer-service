6ee3de6f035f39623ed86d534451495a363c843c
oflorez1381 <oscarflorez1381@gmail.com>
2020-12-03 14:43:26 -0500

    feat(59): Section 5 boostrap data

---
+++ src/main/java/odfd/com/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,46 @@
+package odfd.com.msscbeerservice.bootstrap;
+
+import odfd.com.msscbeerservice.domain.Beer;
+import odfd.com.msscbeerservice.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public final class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if (beerRepository.count() == 0) {
+
+            beerRepository.save(Beer.builder()
+                    .name("Mango Bobs")
+                    .style("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .name("Galaxy Cat")
+                    .style("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+        }
+    }
+}
