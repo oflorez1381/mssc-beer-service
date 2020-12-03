e9c270baa5a79cd931a6b001b208a5510f362ade
oflorez1381 <oscarflorez1381@gmail.com>
2020-12-03 09:05:47 -0500

    feat(43) Section 4 added beer controller with simple operations without implementation

---
+++ src/main/java/odfd/com/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,31 @@
+package odfd.com.msscbeerservice.web.controller;
+
+import odfd.com.msscbeerservice.web.model.BeerDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BeerDTO> getBeerById(@PathVariable UUID id) {
+        //todo: implementation
+        return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDTO beer) {
+        //todo: implementation
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateBeerById(@PathVariable UUID id, @RequestBody BeerDTO beer) {
+        //todo: implementation
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
